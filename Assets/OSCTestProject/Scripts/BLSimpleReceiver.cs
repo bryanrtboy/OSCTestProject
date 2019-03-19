8ef6b49c18e95c3ec3d5cd075bc7887db05dc335
bryanrtboy <bryan@bryanleister.com>
2019-03-19 12:44:23 -0600

    Initial commit

---
+++ Assets/OSCTestProject/Scripts/BLSimpleReceiver.cs
@@ -0,0 +1,128 @@
+﻿//Bryan Leister
+//March 2019
+//
+//Using Jorge Garcia's UnityOSC receiver to control particles
+//https://github.com/jorgegarcia/UnityOSC
+//
+//This example is using a TouchOSC controller running on an iPhone. 
+//
+//Instructions to get the OSC up and running
+//1. Get the TouchOSC app from the app store
+//2. Setup this computer to be a WIFI hub
+//3. Get your computer's IP address from Preferences>Network
+//4. Hook your iPhone up to the WIFI hub you just made
+//5. Open up TouchOSC and set up the IP to match this computer
+//6. This script is using the TouchOSC control setup as Mix 16
+//7. Enjoy!
+//
+//To set this up in Unity, do this:
+//1. This script needs to be on a GameObject in your scene (only one instance of this script should exist in your level)
+//2. Create new scripts that subscribe to the events here, see the ParticleController.cs for an example of how to do this
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityOSC;
+using System;
+
+public class BLSimpleReceiver : MonoBehaviour
+{
+
+    private OSCReciever reciever;
+    public static BLSimpleReceiver instance;
+
+    //These events can be subscribed to by other scripts. Whenever a message comes in to a subscriber, it will get
+    //the value immediately
+    public event Action<float> OnSlider1;
+    public event Action<float> OnSlider2;
+    public event Action<float> OnSlider3;
+    public event Action<float> OnSlider4;
+    public event Action<Vector2> OnXY;
+    public event Action<bool> OnToggle;
+
+    //This was set up using the iOS TouchOSC with the controller Mix16
+    public int port = 8000;
+    public string m_slider1 = "/1/fader1";
+    public string m_slider2 = "/1/fader2";
+    public string m_slider3 = "/1/fader3";
+    public string m_slider4 = "/1/fader4";
+    public string m_xy = "/1/xy";
+    public string m_toggle = "/1/toggle1";
+
+    void Awake()
+    {
+        //Make sure this is in fact the only instance (Singleton pattern)
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        reciever = new OSCReciever();
+        reciever.Open(port);
+    }
+
+    void OnDisable()
+    {
+        if (reciever != null)
+        {
+            Debug.Log("Closing the reciever");
+            reciever.Close();
+        }
+    }
+
+    void Update()
+    {
+        if (reciever.hasWaitingMessages())
+        {
+            OSCMessage msg = reciever.getNextMessage();
+            Debug.Log(string.Format("message received: {0} {1}", msg.Address, DataToString(msg.Data)));
+
+            //Check if the OSC message is from m_slider, and if anyone is listening for Slider1, if so, send them the float values
+            if (msg.Address == m_slider1 && OnSlider1 != null)
+                OnSlider1((float)msg.Data[0]);
+
+            if (msg.Address == m_slider2 && OnSlider2 != null)
+                OnSlider2((float)msg.Data[0]);
+
+            if (msg.Address == m_slider3 && OnSlider3 != null)
+                OnSlider3((float)msg.Data[0]);
+
+            if (msg.Address == m_slider4 && OnSlider4 != null)
+                OnSlider4((float)msg.Data[0]);
+
+            if (msg.Address == m_xy && OnXY != null)
+            {
+                if (msg.Data.Count < 2)
+                {
+                    Debug.LogError("This message is not sending xy coordinate!");
+                    return;
+                }
+                Vector2 m_data = new Vector2((float)msg.Data[0], (float)msg.Data[1]);
+                OnXY(m_data);
+            }
+
+
+            if (msg.Address == m_toggle && OnToggle != null)
+                OnToggle(Convert.ToBoolean(msg.Data[0]));
+        }
+    }
+
+    private string DataToString(List<object> data)
+    {
+        string buffer = "";
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            buffer += data[i].ToString() + " ";
+        }
+
+        buffer += "\n";
+
+        return buffer;
+    }
+}
---
+++ Assets/OSCTestProject/Scripts/ParticleController.cs
@@ -0,0 +1,108 @@
+﻿//Bryan Leister
+//March 2019
+//
+//Grab the values from our OSC reciever and do something with them!
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem m_particles;
+    public Texture2D m_textureGradient;
+
+    void Start()
+    {
+        //This is how you subscribe to events. Whenever the OSC receive gets a message, it will send
+        //the results of that message to any script that subscribes to that event (like this script!)
+        BLSimpleReceiver.instance.OnSlider1 += ReadSlider1;
+        BLSimpleReceiver.instance.OnSlider2 += ReadSlider2;
+        BLSimpleReceiver.instance.OnSlider3 += ReadSlider3;
+        BLSimpleReceiver.instance.OnSlider4 += ReadSlider4;
+        BLSimpleReceiver.instance.OnXY += ReadXY;
+        BLSimpleReceiver.instance.OnToggle += ReadToggle;
+    }
+
+    void OnDisable()
+    {
+        //You should always unsubscribe on disable to clean up the event system
+        BLSimpleReceiver.instance.OnSlider1 -= ReadSlider1;
+        BLSimpleReceiver.instance.OnSlider2 -= ReadSlider2;
+        BLSimpleReceiver.instance.OnSlider3 -= ReadSlider3;
+        BLSimpleReceiver.instance.OnSlider4 -= ReadSlider4;
+        BLSimpleReceiver.instance.OnXY -= ReadXY;
+        BLSimpleReceiver.instance.OnToggle -= ReadToggle;
+    }
+
+    void ReadSlider1(float f)
+    {
+        //Now that we have the slider float value, do something with it
+
+        //Pick a pixel along the x axis of the texture, based on f. 0 = far left side of texture, 1 = far right
+        int x = (int)Mathf.Lerp(0, m_textureGradient.width, f);
+        //Get the color of the pixel, we are coming 2 down on the Y axis
+        Color c = m_textureGradient.GetPixel(x, 2);
+
+        //Color the particle using that pixel
+        var main = m_particles.main;
+        main.startColor = c;
+    }
+
+    //This function changes the brightness of the background
+    void ReadSlider2(float f)
+    {
+        float h, s, v;
+
+        //Get the camera tagged MainCamera's background color
+        Color c = Camera.main.backgroundColor;
+
+        //Get the Hue, Saturation and Value (brightness ) of the color
+        Color.RGBToHSV(c, out h, out s, out v);
+
+        //Based on the slider, change the value
+        c = Color.HSVToRGB(h, s, f);
+
+        //Apply the color to the camera's background
+        Camera.main.backgroundColor = c;
+    }
+
+    void ReadSlider3(float f)
+    {
+        float h, s, v;
+        Color c = Camera.main.backgroundColor;
+        Color.RGBToHSV(c, out h, out s, out v);
+        c = Color.HSVToRGB(h, f, v);
+        Camera.main.backgroundColor = c;
+    }
+
+    void ReadXY(Vector2 formantVector)
+    {
+        //find a position between -3 and 3, if formant.x = 0, position is -3, if formant.x = 1, position is 3
+        float posx = Mathf.Lerp(-3, 3, formantVector.x);
+        float posy = Mathf.Lerp(-3, 3, formantVector.y);
+
+        m_particles.transform.position = new Vector3(posx, posy, m_particles.transform.position.z);
+
+    }
+
+    void ReadSlider4(float f)
+    {
+        float h, s, v;
+        Color c = Camera.main.backgroundColor;
+        Color.RGBToHSV(c, out h, out s, out v);
+        c = Color.HSVToRGB(f, s, v);
+        Camera.main.backgroundColor = c;
+    }
+
+
+
+    void ReadToggle(bool isOn)
+    {
+        var main = m_particles.main;
+        if (isOn)
+            main.simulationSpace = ParticleSystemSimulationSpace.World;
+        else
+            main.simulationSpace = ParticleSystemSimulationSpace.Local;
+    }
+}
